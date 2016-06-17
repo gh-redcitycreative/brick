96fb3f069f467c5ebcb5a5607951e22bd1c35aa9
Glen Hayes <glen@redcitycreative.com>
2016-06-17 12:02:34 -0600

    guzzle added and route binding to estimote api

---
+++ app/Http/Controllers/AnalyticsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class AnalyticsController extends Controller
+{
+//     public function store(){
+// 		$client = new GuzzleHttp\Client();
+// 		$res = $client->request('GET', 'https://cloud.estimote.com/v1/analytics/visits?from=1448928000&to=1451606399&granularity=daily', [
+// 		    'auth' => ['radar-test-web-app-a3s
+// 		', '651a6208cc946b7a105300609a7caa60']
+// 		]);
+// 		return $res->getStatusCode();
+// 	}
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/analytics', function(){
+	$client = new GuzzleHttp\Client();
+	$res = $client->request('GET', 'https://cloud.estimote.com/v1/analytics/visits?from=1448928000&to=1451606399&granularity=daily', [
+	    'auth' => ['radar-test-web-app-a3s', '651a6208cc946b7a105300609a7caa60']
+	]);
+	echo $res->getBody();
+
+});
